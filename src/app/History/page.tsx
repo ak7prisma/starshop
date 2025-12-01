953455189fc1bb70a67280c773c10e610d1aafa7
AKzPrisma <ahmadkurniaprisma@gmail.com>
2025-12-02 00:27:24 +0700

    add-maintenance-page

---
+++ src/app/History/page.tsx
@@ -0,0 +1,11 @@
+import Maintenance from "../components/Maintenance";
+
+
+export default function HistoryPage() {
+    return (
+        <div className="w-full pt-20 md:pt-40 md:pb-10">
+            <Maintenance backLink="/Home" />
+        </div>
+
+    );
+}
--- src/app/components/ui/ProductDetailCard.tsx
+++ src/app/components/ui/ProductDetailCard.tsx
@@ -1,6 +1,15 @@
 import type { Product } from '@/datatypes/productsType'; 
 
 export default function ProductDetailCard({product} : Readonly<{product: Product}>) {
+
+    let input: string;
+    if (product.category === "Games"){
+        input="ID Game";
+    }
+    else {
+        input="No Handphone";
+    }
+    
     return (
         <div className="lg:w-1/3 p-6 bg-[#181B2B] rounded-2xl shadow-xl border border-[#2D3142] h-fit">
             <h2 className="text-2xl font-bold mb-4 text-white border-b border-[#2D3142] pb-3">{product.nameProduct}</h2>
@@ -10,10 +19,10 @@ export default function ProductDetailCard({product} : Readonly<{product: Product
             </div>
 
             <p className="text-gray-300 mb-4 leading-relaxed">
-                Top up {product.nameProduct} secara cepat dan aman. Proses ini memerlukan langkah pengisian ID Game, pemilihan nominal serta verifikasi pembayaran.
+                Top up {product.nameProduct} secara cepat dan aman. Proses ini memerlukan langkah pengisian {input}, pemilihan nominal serta verifikasi pembayaran.
             </p>
             <p className="text-sm text-indigo-400 italic font-medium">
-                Penting: Pastikan ID Game yang dimasukkan sudah benar untuk menghindari kesalahan pengisian.
+                Penting: Pastikan {input} yang dimasukkan sudah benar untuk menghindari kesalahan pengisian.
             </p>
         </div>
     );
