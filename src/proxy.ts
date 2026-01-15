b6ff1dc6c808133fcf41f85d93e42dd465d02ce1
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-01-15 12:11:36 +0700

    chore: add role implementation to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,73 @@
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { updateSession } from '@/app/utils/supabase/proxy'
 
-export async function proxy(request: NextRequest) {
-  const path = request.nextUrl.pathname
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
 
-  if (
-    path.startsWith('/_next/static') ||
-    path.startsWith('/_next/image') ||
-    path === '/favicon.ico' ||
-    new RegExp(/\.(svg|png|jpg|jpeg|gif|webp)$/).exec(path)
-  ) {
-    return NextResponse.next()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            request.cookies.set(name, value)
+          })
+
+          response = NextResponse.next({
+            request,
+          })
+
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options)
+          })
+        },
+      },
+    }
+  )
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+  const url = request.nextUrl
+  const isDashboardRoute = url.pathname.startsWith('/dashboard')
+  const isAuthRoute = url.pathname.startsWith('/login')
+
+  // Logic Keamanan Role login
+
+  if (isDashboardRoute && !user) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  return await updateSession(request)
+  if (isAuthRoute && user) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (isDashboardRoute && user) {
+    const { data: profile } = await supabase
+      .from('profiles')
+      .select('role')
+      .eq('idProfil', user.id)
+      .single()
+
+    const userRole = profile?.role
+
+    if (userRole !== 'admin' && userRole !== 'boss') {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    String.raw`/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)`,
+  ],
 }
