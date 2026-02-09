196b04b61f30819594188fc1cd7e32b740702c2c
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-02-09 14:52:12 +0700

    fix: data about page

---
+++ src/constant/index.ts
@@ -0,0 +1,13 @@
+export const storeData = {
+    tagline: "Layanan top-up game dan produk digital MURAH x TERPERCAYA",
+    tagline2: "Online Top-up Store — Game & Digital Products",
+    description: "Starshop menyediakan layanan top-up game dan produk digital lainnya dengan harga terjangkau dan pelayanan cepat. Kami melayani pembelian pulsa, paket data, voucher game, dan item digital untuk berbagai platform. Setelah pembayaran, pelanggan dapat mengunggah bukti bayar untuk diproses melaluisistem kami.",
+};
+
+export const serviceData = [
+    "Top-up game (voucher & in-game currency)",
+    "Pulsa & Token Listrik",
+    "Pembayaran via transfer & upload bukti",
+    "Customer support via Instagram / WhatsApp",
+    "Full Control Website via admin dashboard"
+]
