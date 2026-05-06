8fb9fd23bb0a93bfb9feefab3c3c77e7156f6a42
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-05-06 09:32:17 +0700

    chore: update link contact

--- src/constant/socialdata.ts
+++ src/constant/socialdata.ts
@@ -1,8 +1,8 @@
 import { FaEnvelope, FaGithub, FaInstagram, FaMapMarkedAlt, FaWhatsapp } from "react-icons/fa"
 
 export const socialLinks = [
-    {label: "Instagram", value: "akprisma", href: "https://www.instagram.com/akprisma?igsh=MTJtd2lwaHZoeXFrZA==", icon: FaWhatsapp},
-    {label: "WhatsApp", value: "+628989209565", href: "https://wa.me/qr/424AF5XR3VZ7B1", icon: FaInstagram},
+    {label: "Instagram", value: "akprisma", href: "https://www.instagram.com/akprisma?igsh=MTJtd2lwaHZoeXFrZA==", icon: FaInstagram},
+    {label: "WhatsApp", value: "+628989209565", href: "https://wa.me/qr/424AF5XR3VZ7B1", icon: FaWhatsapp},
     {label: "Github", value: "Github", href: "https://github.com/ak7prisma", icon: FaGithub},
     {label: "Email", value: "ahmadkurniaprisma@gmail.com", href: "https://mail.google.com/mail/u/0/#inbox?compose=jrjtXDxrKGgKwqjWRmXZtsfZJbNBzSDJmfPvVgTxFTJSqcppTFqKXdpnCLnnqjZWFFbxTKMW", icon:FaEnvelope}
 ]
